08febe866b5127b76f56ec809d1a7c233936ba70
Mr.Tracky <>
2018-11-26 17:33:40 +0800

    Feature:TRPrintJsonLog添加一个类，用于控制台格式化json，便于阅读

---
+++ TRCommonTools/TRCommonTools/TRPrintJsonLog.h
@@ -0,0 +1,17 @@
+//
+//  TRPrintJsonLog.h
+//  MajorProject
+//
+//  Created by Tracky on 2018/10/22.
+//  Copyright © 2018年 HangZhou Major. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRPrintJsonLog : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
